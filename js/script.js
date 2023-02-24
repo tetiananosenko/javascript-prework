27a4590eb93ff8ec3999dc481ea2e8b14f2acaea
Tetiana Nosenko <tanya2016english@gmail.com>
2023-02-24 13:05:47 +0100

    Create computer's move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('The number drawn is: ' + randomNumber);
+
+let computerMove = 'unknown move';
+
+if (randomNumber == 1) {
+    computerMove = 'stone';
+} else if (randomNumber == 2) {
+    computerMove = 'paper';
+} else if (randomNumber == 3) {
+    computerMove = 'scissors';
+}
+
+printMessage('My move: ' + computerMove);
+
 let playerInput = prompt('Choose your move! 1: stone, 2: paper, 3: scissors.');
 
 console.log('The player entered: ' + playerInput);
