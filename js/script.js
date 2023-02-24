a71c16038d917ee7025e77a3638a4be2bf9e69ca
Tetiana Nosenko <tanya2016english@gmail.com>
2023-02-24 12:42:08 +0100

    Create player's move

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-let playerMove = 'papier';
+let playerInput = prompt('Choose your move! 1: stone, 2: paper, 3: scissors.');
+
+console.log('The player entered: ' + playerInput);
+
+let playerMove = 'unknown move';
+
+if (playerInput == '1') {
+    playerMove = 'stone';
+} else if (playerInput == '2') {
+    playerMove = 'paper';
+} else if (playerInput == '3') {
+    playerMove = 'scissors';
+}
+
+printMessage('Your move: ' + playerMove);
